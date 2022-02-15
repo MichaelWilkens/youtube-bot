39490c0d2b6cc587cd40e1bcf41d7c7ede063a77
Michael Wilkens <michaelwilkens@Michaels-MacBook-Pro.local>
2022-02-15 16:18:53 -0500

    update not logic

--- bot.js
+++ bot.js
@@ -1,5 +1,11 @@
 const open = require('open');
 
+// open web page 
+open('https://www.youtube.com/watch?v=t9uwMwAo9yU');
+
+let counter = 0;
+
+
 // perform loop actoun 
 setInterval(() => {
     // you can select which browser to use by 
@@ -9,7 +15,20 @@ setInterval(() => {
 
     // skipping the second parameter just opens in your default browser
 
-    open('https://www.youtube.com/watch?v=t9uwMwAo9yU');
+    // web browsers typically don't allow opening more than 100 tabs, so after 
+    // 100 interations, change to a different browser
+    if (counter <= 100) {
+        open('https://www.youtube.com/watch?v=t9uwMwAo9yU');
+    }
+    if (counter > 100 && counter < 200) {
+    open('https://www.youtube.com/watch?v=t9uwMwAo9yU', { app: { name: 'safari' } });
+    }
+    else {
+        open('https://www.youtube.com/watch?v=t9uwMwAo9yU', { app: { name: 'safari' } });
+    }
+
+    // increase counter
+    counter++;
 
     // this is the number of milliseconds. The video is 5 min 30 secs, which is about 
     // 330 seconds, which is 330000 in milliseconds, plus 5 second buffer

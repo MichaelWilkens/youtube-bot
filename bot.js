6eb7dec8d7aeac9cc086da247e45df840ba5ce83
Michael Wilkens <michaelwilkens@Michaels-MacBook-Pro.local>
2022-02-15 16:06:00 -0500

    open in default browser

--- bot.js
+++ bot.js
@@ -4,9 +4,16 @@ const open = require('open');
 setInterval(() => {
     // you can select which browser to use by 
     // changing "chrome" to "safari", "firefox" etc.
-    open('https://www.youtube.com/watch?v=t9uwMwAo9yU', { app: { name: 'chrome' } });
-// this is the number of milliseconds. The video is 5 min 30 secs, which is about 
-// 330 seconds, which is 330000 in milliseconds, plus 5 second buffer
+
+    // open('https://www.youtube.com/watch?v=t9uwMwAo9yU', { app: { name: 'chrome' } });
+
+    // skipping the second parameter just opens in your default browser
+
+    open('https://www.youtube.com/watch?v=t9uwMwAo9yU');
+
+    // this is the number of milliseconds. The video is 5 min 30 secs, which is about 
+    // 330 seconds, which is 330000 in milliseconds, plus 5 second buffer
+    // }, 335000
 }, 335000
 );
 

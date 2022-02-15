8a4552f92ec05bd4d1fc0100aa7f3e88c31be265
Michael Wilkens <michaelwilkens@Michaels-MacBook-Pro.local>
2022-02-15 16:03:25 -0500

    initial push

---
+++ bot.js
@@ -0,0 +1,13 @@
+const open = require('open');
+
+// perform loop actoun 
+setInterval(() => {
+    // you can select which browser to use by 
+    // changing "chrome" to "safari", "firefox" etc.
+    open('https://www.youtube.com/watch?v=t9uwMwAo9yU', { app: { name: 'chrome' } });
+// this is the number of milliseconds. The video is 5 min 30 secs, which is about 
+// 330 seconds, which is 330000 in milliseconds, plus 5 second buffer
+}, 335000
+);
+
+
